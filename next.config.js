2492e20e6166b5be80340f8b3fed93d93d73edc9
Dmytro <dmytro.osiychuk@gmail.com>
2023-07-25 16:06:34 +0200

    Refactor for SQLite2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ['bcrypt']
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreBuildErrors: true
+  }
+}
 
 module.exports = nextConfig
